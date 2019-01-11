dac1e46d9a9046cd4f4c484bd4aed743fb0339e2
Emmanuel Anzele <eanzele@gmail.com>
2019-01-11 10:33:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 
 # code your #position_taken? method here!
 
-def #position_taken? (board, index)
-  if board[index] == "X" || board[index] == "O"
-    TRUE
+def position_taken?(board, index)
+  if board[index] == "x" || board[index] == "o"
+    true
   else
-    FALSE
+    false
   end
 end
