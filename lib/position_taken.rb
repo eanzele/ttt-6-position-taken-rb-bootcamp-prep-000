2c716cb64f8d28ddca87a58d938011464602198c
Emmanuel Anzele <eanzele@gmail.com>
2019-01-11 10:34:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == "x" || board[index] == "o"
+  if board[index] == "X" || board[index] == "O"
     true
   else
     false
